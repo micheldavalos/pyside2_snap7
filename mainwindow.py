cb2b606fda6fdd8d012f8f8404234504b83d9424
Michel Davalos <michel.davalos@academicos.udg.mx>
2021-12-29 10:48:35 -0600

    mainwindow.ui created with label and lineEdit to show nombre varible

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QMainWindow
-
+from ui_mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
